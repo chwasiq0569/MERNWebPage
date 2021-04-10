b7aac08515a1964c94d642eaa47d4f110b831b8b
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-10 10:28:21 +0500

    Created Register Route using Async Await

--- router/auth.js
+++ router/auth.js
@@ -36,5 +36,29 @@ router.post("/register", (req, res) => {
        })
     })
 })
-
+// using async await
+// router.post("/register", async (req, res) => {
+//     console.log(req.body)
+//     const {name, email, phone, work, password, confirm_password} = req.body;
+//     if(!name || !email ||!phone ||!work ||!password ||!confirm_password){
+//       return res.status(422).json({error: "Please fill the required fields."})
+//     }
+//     try{
+//         const userExists = await User.findOne({email: email})
+//         if(userExists){
+//                 return res.status(422).json({error: "User Already Exists"})
+//         }
+//         const user = new User({name, email, phone, work, password, confirm_password})
+//         if(user){
+//             try{
+//                 const savedUser = await user.save();
+//                 if(savedUser) return res.status(201).json({message: "User Created Successfully."})
+//             } catch(err){
+//                   return res.status(500).json({error: "Registration Failed."})
+//             }
+//         }
+//        } catch(err){
+//            console.log(err)
+//        }
+// })
 module.exports = router
