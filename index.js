3b3b3b1648052e51f7c55669679cf8db2bd136c9
chwasiq0569 <samsunggrandprimeplus056@gmail.com>
2021-04-10 10:27:25 +0500

    Created Register Route & added Ability to save data to databse

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(express.json())
 
 require("./db/conn")
 
+
 app.use(require('./router/auth.js'))
 
 
--- router/auth.js
+++ router/auth.js
@@ -2,12 +2,39 @@ const express = require("express")
 
 const router = express.Router()
 
+require("../db/conn")
+
+const User = require("../model")
+
 router.get("/", (req, res) => {
-    res.send("INSIDE AUTH")
+
+   return res.send("INSIDE AUTH")
+
 })
+
 router.post("/register", (req, res) => {
     console.log(req.body)
-    res.json({message: req.body})
+
+    const {name, email, phone, work, password, confirm_password} = req.body;
+
+    if(!name || !email ||!phone ||!work ||!password ||!confirm_password){
+      return res.status(422).json({error: "Please fill the required fields."})
+    }
+
+    User.findOne({email: email}).then((userExists) => {
+        if(userExists) {
+          return res.status(422).json({error: "User Already Exists"})
+        }
+       const user = new User({
+           name, email, phone, work, password, confirm_password
+       })
+       
+       user.save().then(() => {
+          return res.status(201).json({message: "User Created Successfully."})
+       }).catch(() => {
+          return res.status(500).json({error: "Registration Failed."})
+       })
+    })
 })
 
 module.exports = router
